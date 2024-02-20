12aa12f148c13a8d76b1a5b40f255857e7948955
pikxs08 <76698164+pikxs08@users.noreply.github.com>
2024-02-20 06:57:58 +0200

    stores favourites

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const fs = require("fs");
 
 // Ensure body parser is running
 const bodyParser = require("body-parser");
+const { useEffect } = require("react");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -15,6 +16,64 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/api", (req, res) => {
+  fs.readFile("favorites.json", (err, data) => {
+    if (err) {
+      res.status(500).send("Error getting data");
+    } else {
+      const jsonData = JSON.parse(data);
+      res.send(jsonData);
+    }
+  });
+});
+
+function getFavorites() {
+  try {
+    const favorites = fs.readFileSync("favorites.json");
+    return JSON.parse(favorites);
+  } catch (e) {
+    // file non-existent
+    fs.writeFileSync("favorites.json", "[]");
+    return [];
+  }
+}
+
+function addFavorite(id, title, artistName, img) {
+  const favorites = getFavorites();
+  favorites.push({
+    id: id,
+    title: title,
+    artistName: artistName,
+    img: img,
+  });
+  fs.writeFileSync("favorites.json", JSON.stringify(favorites));
+}
+
+app.post("/post", (req, res) => {
+  const favorites = getFavorites();
+  const newTitle = req.body.title;
+  const newArtistName = req.body.artistName;
+  const newImg = req.body.img;
+
+  // Check if the project already exists
+  const favoriteExists = favorites.some(
+    (favorite) => favorite.title === newTitle
+  );
+
+  if (favoriteExists) {
+    res.status(200).json({ message: "Favorite already exists" });
+  } else {
+    addFavorite(favorites.length + 1, newTitle, newArtistName, newImg);
+    res.status(200).json({ message: "Favorite added" });
+  }
+});
+
+app.delete("/delete", (req, res) => {
+  const favorites = getFavorites();
+  const title = req.body.title;
+
+  const newFavorites = favorites.filter((favorite) => favorite.title !== title);
+
+  fs.writeFileSync("favorites.json", JSON.stringify(newFavorites));
+  res.status(200).json({ message: "Favorite deleted" });
 });
