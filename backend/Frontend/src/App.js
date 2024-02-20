f7a5edaa5770beaec58b0ff4d57a6ff80bfb6f7c
pikxs08 <76698164+pikxs08@users.noreply.github.com>
2024-02-20 07:42:09 +0200

    Added frontend

---
+++ backend/Frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>iTunes Search App</h1>
+      <p>Search for your favorite music, movies, and TV shows!</p>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
