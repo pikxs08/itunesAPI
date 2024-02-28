9f5ecc1efda9c0b214ca43429b80627fba4ac65e
pikxs08 <76698164+pikxs08@users.noreply.github.com>
2024-02-28 08:18:23 +0200

    changed Port to 5000

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
