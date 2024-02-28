d8fa929eb8a12bd5451b6c2d0327b6b78041fdf8
pikxs08 <76698164+pikxs08@users.noreply.github.com>
2024-02-28 22:22:44 +0200

    npm install express

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
